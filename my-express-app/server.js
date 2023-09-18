420e7d58a53f283d4966885c9aa5b7f02630a42f
Isaac-Chow <99174054+Isaac-Chow@users.noreply.github.com>
2023-09-18 11:47:28 +0000

    new stuff

--- my-express-app/server.js
+++ my-express-app/server.js
@@ -1,15 +1,40 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
 const app = express();
 const PORT = 3000;
-
-const bodyParser = require('body-parser');
-
 app.use(bodyParser.json());
-
-const fs = require('fs');
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.post('/data', (req, res) => {
+    const newData = req.body;
+    fs.readFile('database.json', 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Failed to read the database.' });
+        }
+        const database = JSON.parse(data);
+        database.data.push(newData);
+        fs.writeFile('database.json', JSON.stringify(database, null, 2), err => {
+            if (err) {
+                return res.status(500).json({ error: 'Failed to write to the database.' });
+            }
+            res.json(newData);
+        });
+    });
+});
+
+app.get('/data', (req, res) => {
+    fs.readFile('database.json', 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Failed to read the database.' });
+        }
+        const database = JSON.parse(data);
+        res.json(database.data);
+    });
 });
 
 app.listen(PORT, () => {
